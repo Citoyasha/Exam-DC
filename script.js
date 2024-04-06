41483d457fe51d6fb565ad3879d39358ee2c3bcd
citoyasha <citoyasha@mail.ru>
2024-04-06 15:31:27 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function on() {
+    console.log(document.getElementById("overlay").style.display);
+    document.getElementById("overlay").style.display = "block";
+  }
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+  } 
